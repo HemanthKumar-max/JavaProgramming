3bd6c5440cff52e7959c2f80e51f5a7220dd5d8a
hemanth <hemanthkumarr765@gmail.com>
2020-02-07 23:43:46 +0530

    [hemanth] add CustomizedException class

---
+++ CustomizedException.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.fellowship.Functional;
+
+public class CustomizedException extends Exception
+{
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public CustomizedException(String s)
+{
+	super(s);
+}
+}
